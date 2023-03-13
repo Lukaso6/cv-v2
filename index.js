e655e5a6bc5073f225e24e6fb551c6b657896db9
Lukaso6 <112543673+Lukaso6@users.noreply.github.com>
2023-03-13 05:20:02 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/* parallax */
+
+document.addEventListener("scroll", ()=> {
+    let real = document.documentElement.scrollTop
+
+    let elems = document.querySelectorAll(".parralax_container > *")
+
+
+    for (const [idx, elem] of elems.entries()) {
+        let scroll_factor = (elem.getAttribute("data-speed")/100)*-1 +1
+        elem.style = `bottom: -${scroll_factor*real}px`
+    }
+})
